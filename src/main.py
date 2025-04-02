fee4f85bfd68354a0581c1b5ba26b86aeefa8f4a
Sebastian Zamorano <szamorano@fullpay.cl>
2025-04-02 20:31:13 -0300

    se agrega opciones por error

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ logging.basicConfig(level=logging.INFO)
 Logger = logging.getLogger(__name__)
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
